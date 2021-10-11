a3c3120c9896fceed1311e11811855c7c102cfa9
Sudip Panja <panja370@gmail.com>
2021-10-11 08:35:24 +0530

    Added : Welcome Message

---
+++ src/main/java/com/bridgelabz/AddressBookService.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To create address book
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-10-11
+ */
+public class AddressBookService {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Address Book Program");
+    }
+}
